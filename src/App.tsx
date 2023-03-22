8438b765d24bd8be9b05b0318bff9b31652539af
pragkb <kbprabu6@gmail.com>
2023-03-22 21:09:05 +0530

    adding restrict functions(2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 
 export type CommonResponse = {
   status?: 'danger' | 'success';
@@ -8,16 +6,31 @@ export type CommonResponse = {
 
 export const Utilities = () => {
 
-  //To restrict Special characters in input fields
-  // restrictSpecialCharacters(e, value, length) {
-  //   try {
-  //     if (/^[a-zA-Z0-9, \s]*$/.test(e.key) && value.length < length) {
-  //       return true;
-  //     } else {
-  //       e.preventDefault();
-  //       return false
-  //     }
-  //   } catch (e) {}
-  // }
-  // return 
+  // To restrict Special characters in input fields
+  function restrictSpecialCharacters(e:any, value:any, length:number) {
+    try {
+      if (/^[a-zA-Z0-9, \s]*$/.test(e.key) && value.length < length) {
+        return true;
+      } else {
+        e.preventDefault();
+        return false
+      }
+    } catch (e) {}
+  }
+
+  //To restrict numbers & special characters in input field
+  function restrictNumberSpecialCharacters (e:any, value:any, length:number) {
+    try{
+      if (/^[a-zA-Z\s]*$/.test(e.key) && value.length < length) {
+        return true;
+      } else {
+        e.preventDefault();
+        return false;
+      }
+    } catch (e) {}
+  }
+  return {
+    restrictSpecialCharacters,
+    restrictNumberSpecialCharacters
+  }
 }
